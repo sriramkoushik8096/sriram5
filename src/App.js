dbf5910c15fa78a9cf7d605972acf1b93ce69c74
Sri ram Koushik Batchu <sriram.koushikbatchu2021@vitstudent.ac.in>
2023-10-14 13:32:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AgeCalculator from './AgeCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
+
